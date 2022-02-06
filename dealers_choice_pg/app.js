26a576ec14982d06b1dc5b322ce3bb61e18612a5
Ice Tam <icetam4@gmail.com>
2022-02-06 15:19:00 -0500

    updates

--- dealers_choice_pg/app.js
+++ dealers_choice_pg/app.js
@@ -1,1 +1,16 @@
 const pg = require('pg')
+const express = require('express')
+const app = express();
+const port = 3002; 
+
+const client = new pg.Client('postgres://localhost/dealers_choice_db')
+
+const setUp = async ()=> {
+    await client.connect();
+}
+setUp();
+
+
+
+
+app.listen(port, ()=> console.log(`listening on port ${port}`));
