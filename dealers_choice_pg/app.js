9064d91679301075c402c1a33fbcb4adaf977ffd
Ice Tam <icetam4@gmail.com>
2022-02-06 22:26:33 -0500

    updates

--- dealers_choice_pg/app.js
+++ dealers_choice_pg/app.js
@@ -1,16 +1,48 @@
-const pg = require('pg')
+const { client, syncAndSeed } = require('./db/datab.js')
 const express = require('express')
 const app = express();
-const port = 3002; 
+const port = 3001; 
+const path = require('path');
 
-const client = new pg.Client('postgres://localhost/dealers_choice_db')
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', async (req, res, next) => {
+    const response = await client.query('SELECT * FROM icecream');
+    const icecream = response.rows;
+    res.send(`
+     <html>
+       <head>
+        <link rel ='stylesheet' href='/public/style.css'/>
+       </head>
+       <body>
+       <h1> Van Leeuwen Ice Cream Flavors </h1>
+       <ul>
+         ${
+             icecream.map (flavor => `
+              <li>
+                <a href= '/icecream/${icecream.id}'>
+                ${ flavor.name }
+                </a>
+              </li>
+             `).join("")
+         }
+       </ul>
+       </body>
+     </html>
+        
+    `);
+
+    next();
+})
+
 
 const setUp = async ()=> {
-    await client.connect();
-}
-setUp();
-
-
+        await client.connect();
+        await syncAndSeed();
+        console.log('connected to db')
+    }
+    setUp();
 
 
 app.listen(port, ()=> console.log(`listening on port ${port}`));
---
+++ dealers_choice_pg/db/datab.js
@@ -0,0 +1,49 @@
+const pg = require('pg')
+
+const client = new pg.Client('postgres://localhost/dealers_choice_db')
+
+const syncAndSeed = async () => {
+    const SQL = `
+    DROP TABLE IF EXISTS "info";
+    DROP TABLE IF EXISTS "icecream";
+    
+    CREATE TABLE icecream (
+        id INTEGER PRIMARY KEY, 
+        name VARCHAR(100) 
+    );
+
+    CREATE TABLE info (
+        id INTEGER PRIMARY KEY,
+        description VARCHAR (500),
+        cost INTEGER,
+        desc_id INTEGER REFERENCES icecream(id)
+
+    );
+
+
+    INSERT INTO icecream(id, name) VALUES (1, 'Salted Caramel');
+    INSERT INTO icecream(id, name) VALUES (2, 'Cookies & Cream Caramel Swirl');
+    INSERT INTO icecream(id, name) VALUES (3, 'Sicilian Pistachio');
+    INSERT INTO icecream(id, name) VALUES (4, 'Honeycomb');
+    INSERT INTO info(id, description, desc_id, cost) VALUES (1, 'Salty like pure cane sugar caramelized in small copper pots with cold ground vanilla and a touch of sea salt', 1, 12.00);
+    INSERT INTO info(id, description, desc_id, cost) VALUES (2, 'Cream-filled dark chocolate cookies, folded into creamy vegan base. A touch of cold-ground Tahitian vanilla and a swirl of housemade caramel', 2, 12.00);
+    INSERT INTO info(id, description, desc_id, cost) VALUES (3, 'These pistachios are only found on Mount Etna, folded into a creamy cashew milk base', 3, 12.00);
+    INSERT INTO info(id, description, desc_id, cost) VALUES (4, 'Not made with honeycombs or honey, made with a chewy, crunchy caramel candy and folded into vanilla caramel base', 4, 12.00);
+    
+    `;
+
+
+    await client.query(SQL);
+};
+
+// const setUp = async ()=> {
+//     await client.connect();
+//     await syncAndSeed();
+//     console.log('connected to db')
+// }
+// setUp();
+
+module.exports = {
+    client,
+    syncAndSeed
+};
